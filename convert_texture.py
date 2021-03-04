6595479acccafac4f228e5adbc22e958c03fa9ef
TiredHobgoblin <46323898+TiredHobgoblin@users.noreply.github.com>
2021-03-04 15:06:31 -0500

    Update convert_texture.py

--- convert_texture.py
+++ convert_texture.py
@@ -25,10 +25,14 @@ if __name__ == '__main__':
     else:
         raise Exception('Unknown file format given.')
 
+    with open(settings.header_list_name) as f:
+        content = [i.strip() for i in f.readlines()]
+
     # Getting texture
-    uid = gf.get_hash_from_file(settings.header_file_name)
-    texture = texlib.TextureHeader(uid=uid, afi=afi)
-    if file_type == 'dds':
-        texlib.tex2dds(texture, f'{settings.output_directory}/{texture.name}.dds')
-    else:
-        texlib.tex2other(texture, f'{settings.output_directory}/{texture.name}.dds', file_type)
+    for header_file_name in content:
+        uid = gf.get_hash_from_file(header_file_name)
+        texture = texlib.TextureHeader(uid=uid, afi=afi)
+        if file_type == 'dds':
+            texlib.tex2dds(texture, f'{settings.output_directory}/{texture.name}.dds')
+        else:
+            texlib.tex2other(texture, f'{settings.output_directory}/{texture.name}.dds', file_type)
